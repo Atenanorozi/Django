b9909c020f48b29ba4c13acf11247c7c0dd80536
Atena Norozi <atenanorozi359@gmail.com>
2025-12-31 19:34:24 +0330

    add website/urls.py

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,10 @@
-from django.contrib import admin
-from django.urls import *
-
+from django.urls import path
+from website.views import *
 
 urlpatterns = [
-    
-    
+    path('home', index_view)
+    path('about', about_view)
+    path('contact', contact_view)
+
+
 ]
