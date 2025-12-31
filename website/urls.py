fdf6ab4f5be9a03eff5367799fb87c20db9781a5
Atena Norozi <atenanorozi359@gmail.com>
2025-12-31 19:43:08 +0330

    website/urls.py

--- website/urls.py
+++ website/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from website.views import *
 
 urlpatterns = [
-    path('home', index_view)
-    path('about', about_view)
+    path('home', index_view),
+    path('about', about_view),
     path('contact', contact_view)
 
 
